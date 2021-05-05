cea0ac7fba27d81690a42ba86683b0c30256c6b9
shubham-ghuge <shubhamghuge34@gmail.com>
2021-05-05 19:33:06 +0530

    fixed image redering

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,27 +2,33 @@ import { Link } from "react-router-dom";
 
 export const Home = () => {
   const categories = [
-    { name: "honey", url: honey },
-    { name: "nonVeg", url: nonVeg },
-    { name: "masala", url: masala },
-    { name: "veg", url: veg },
-    { name: "eggs", url: eggs },
-    { name: "fruit", url: fruit },
-    { name: "coffee", url: coffee }
+    { name: "honey", url: "honey.png" },
+    { name: "nonVeg", url: "ham.png" },
+    { name: "masala", url: "masala.png" },
+    { name: "veg", url: "food.png" },
+    { name: "eggs", url: "eggs.png" },
+    { name: "fruit", url: "watermelon.png" },
+    { name: "coffee", url: "coffee.png" }
   ];
 
   return (
     <>
       <section className="hero">
         <Link to="/store">
-          <img src={`https://api-farmers-grocery.herokuapp.com/images/banner.jpg`} alt="banner" />
+          <img
+            src={`https://api-farmers-grocery.herokuapp.com/images/banner.jpg`}
+            alt="banner"
+          />
         </Link>
       </section>
       <div className="category-layout">
         {categories.map((item, idx) => (
           <Link to="/store" key={idx}>
             <figure className="category-card">
-              <img src={`https://api-farmers-grocery.herokuapp.com/images/${item.url}.png`} alt="category" />
+              <img
+                src={`https://api-farmers-grocery.herokuapp.com/images/${item.url}`}
+                alt="category"
+              />
               <figcaption>{item.name}</figcaption>
             </figure>
           </Link>
