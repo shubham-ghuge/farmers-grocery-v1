d05b931d9043d9ff0b98199c57e0513bbf2d5b51
shubham-ghuge <shubhamghuge34@gmail.com>
2021-05-05 19:43:33 +0530

    fixed cart styling

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -35,109 +35,111 @@ export const Cart = () => {
 
   return (
     <>
-      {productsInCart.length ? (
-        <div className="cart-layout nav-adjust">
-          <div className="cart">
-            <div className="cart-heading">
-              <h3>product</h3>
-              <h3>quantity</h3>
-              <h3>price</h3>
-            </div>
-            {productsInCart.map(
-              ({
-                _id,
-                name,
-                imgUrl,
-                description,
-                price,
-                discount,
-                quantity
-              }) => (
-                <div className="product-card" key={_id}>
-                  <div
-                    className="product-details"
-                    style={{ cursor: "pointer" }}
-                    onClick={() => navigation(`/product/${_id}`)}
-                  >
-                    <img className="product-img" src={imgUrl} alt="product" />
-                    <div className="wrapper">
-                      <h3 className="product-title">
-                        {name}
-                        <span> 1kg</span>
-                      </h3>
-                      <h4 className="product-price">
-                        ₹{(price - (price * discount) / 100).toFixed(2)}
-                        <span className="strike">₹ {price}.00</span>
-                      </h4>
-                      <h4 className="product-discount">
-                        Discount: {discount}%
-                      </h4>
-                      <p className="product-description">{description}</p>
-                      <p className="d-block d-sm-none"></p>
+      <div className="cart-layout nav-adjust">
+        {productsInCart.length ? (
+          <>
+            <div className="cart">
+              <div className="cart-heading">
+                <h3>product</h3>
+                <h3>quantity</h3>
+                <h3>price</h3>
+              </div>
+              {productsInCart.map(
+                ({
+                  _id,
+                  name,
+                  imgUrl,
+                  description,
+                  price,
+                  discount,
+                  quantity
+                }) => (
+                  <div className="product-card" key={_id}>
+                    <div
+                      className="product-details"
+                      style={{ cursor: "pointer" }}
+                      onClick={() => navigation(`/product/${_id}`)}
+                    >
+                      <img className="product-img" src={imgUrl} alt="product" />
+                      <div className="wrapper">
+                        <h3 className="product-title">
+                          {name}
+                          <span> 1kg</span>
+                        </h3>
+                        <h4 className="product-price">
+                          ₹{(price - (price * discount) / 100).toFixed(2)}
+                          <span className="strike">₹ {price}.00</span>
+                        </h4>
+                        <h4 className="product-discount">
+                          Discount: {discount}%
+                        </h4>
+                        <p className="product-description">{description}</p>
+                        <p className="d-block d-sm-none"></p>
+                      </div>
                     </div>
-                  </div>
-                  <div className="quantity">
-                    <button
-                      className="btn-outline-danger"
-                      onClick={() => {
-                        dispatch({
-                          type: "DECREMENT",
-                          payload: {
-                            pid: _id,
-                            pqty: quantity - 1
+                    <div className="quantity">
+                      <button
+                        className="btn-outline-danger"
+                        onClick={() => {
+                          dispatch({
+                            type: "DECREMENT",
+                            payload: {
+                              pid: _id,
+                              pqty: quantity - 1
+                            }
+                          });
+                          if (quantity === 1) {
+                            toast(`${name} is removed from cart`);
                           }
-                        });
-                        if (quantity === 1) {
-                          toast(`${name} is removed from cart`);
+                        }}
+                      >
+                        -
+                      </button>
+                      <span>{quantity}</span>
+                      <button
+                        className="btn-outline-primary"
+                        onClick={() =>
+                          dispatch({
+                            type: "INCREMENT",
+                            payload: { pid: _id, pqty: quantity + 1 }
+                          })
                         }
-                      }}
-                    >
-                      -
-                    </button>
-                    <span>{quantity}</span>
-                    <button
-                      className="btn-outline-primary"
-                      onClick={() =>
-                        dispatch({
-                          type: "INCREMENT",
-                          payload: { pid: _id, pqty: quantity + 1 }
-                        })
-                      }
-                    >
-                      +
-                    </button>
+                      >
+                        +
+                      </button>
+                    </div>
+                    <h3 className="final-price">₹25.00</h3>
                   </div>
-                  <h3 className="final-price">₹25.00</h3>
-                </div>
-              )
-            )}
-          </div>
+                )
+              )}
+            </div>
 
-          <div className="checkout">
-            <h3 className="muted">
-              You are saving <span>₹ {totalSaved()}</span>
-            </h3>
-            <button
-              className="coupen"
-              onClick={() => setCoupen((prev) => (prev = !prev))}
-            >
-              {coupen ? "applied!" : "Apply coupen!"}
-            </button>
-            <div className="wrapper">
-              <p className="muted">Delivery Charges</p>
-              <p className="muted">₹ 50.00</p>
+            <div className="checkout">
+              <h3 className="muted">
+                You are saving <span>₹ {totalSaved()}</span>
+              </h3>
+              <button
+                className="coupen"
+                onClick={() => setCoupen((prev) => (prev = !prev))}
+              >
+                {coupen ? "applied!" : "Apply coupen!"}
+              </button>
+              <div className="wrapper">
+                <p className="muted">Delivery Charges</p>
+                <p className="muted">₹ 50.00</p>
+              </div>
+              <div className="wrapper">
+                <p className="muted">Total Payable</p>
+                <h2>₹ {coupen ? cartTotal() - 5 : cartTotal()}</h2>
+              </div>
+              <button className="btn-success">Checkout</button>
             </div>
-            <div className="wrapper">
-              <p className="muted">Total Payable</p>
-              <h2>₹ {coupen ? cartTotal() - 5 : cartTotal()}</h2>
-            </div>
-            <button className="btn-success">Checkout</button>
-          </div>
-        </div>
-      ) : (
-        <Jumbotron text="Cart" />
-      )}
-      <ToastContainer />
+          </>
+        ) : (
+          <Jumbotron text="Cart" />
+        )}
+        <ToastContainer />
+      </div>
     </>
   );
 };
